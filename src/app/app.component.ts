df79b4652223d18da11e12e59e49731c116cf4ac
Patrick Neal <patn889@gmail.com>
2018-10-11 09:43:02 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'a9d-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'a9d';
+}
